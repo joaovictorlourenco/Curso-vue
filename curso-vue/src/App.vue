951f2b60768748cd491645d740313b00a81731cf
João Victor Lourenço Barbosa Bernardo Diniz <87540912+joaovictorlourenco@users.noreply.github.com>
2022-11-17 18:50:31 -0300

    primeiro-commit

---
+++ curso-vue/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <NavBar/>
+</template>
+
+<script>
+// Assim que se importa um componente de vue
+import NavBar from '@/components/NavBar'
+
+export default {
+    // Aqui estou exportando o componente que estou
+   name:'App',
+    //Aqui chamo o componente que estou importando
+   components:{
+    NavBar
+   }
+}
+</script>
